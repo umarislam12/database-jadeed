73f40d24213c07abf0f166a75eb56f7fcc05c047
obnox <obnox@DESKTOP-KOIF7PI>
2022-01-09 06:54:58 +0500

    added first domainmodel

---
+++ POS/Domain/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class Product
+    {
+        public Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductNumber { get; set; }
+        public string Description { get; set; }
+        public int QtyStock { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+        public int Cost { get; set; }
+        public string Vandor { get; set; }
+        public int WholesalePrice { get; set; }
+        public int RetailPrice { get; set; }
+        public bool Packed { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
