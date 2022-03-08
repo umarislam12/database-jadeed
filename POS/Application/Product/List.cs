758cac4937bccbd9594e563107d4ec1526caba92
obnox <obnox@DESKTOP-KOIF7PI>
2022-03-09 00:13:51 +0500

    added to application folder

---
+++ POS/Application/Product/List.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Items
+{
+    public class List
+    {
+        public class Query : IRequest<List<Product>> { }
+        public class Handler : IRequestHandler<Query, List<Product>>
+        {
+            public async Task<List<Product>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
