21a78db41605382f7c997318321f723ef8cbd3ad
obnox <obnox@DESKTOP-KOIF7PI>
2022-03-20 02:02:35 +0500

    Revert "Added costs to db"
    This reverts commit c0273f799958b4be6bf88c067eef9b19266ba698.

---
+++ POS/Application/Product/List.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Items
+{
+    public class List
+    {
+        public class Query : IRequest<List<Product>> { }
+        public class Handler : IRequestHandler<Query, List<Product>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Product>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Products.ToListAsync();
+            }
+        }
+    }
+}
